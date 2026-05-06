395a36e3efd2b2f7e55be01c3974a27834337a96
cgbailey98 <claybai98@gmail.com>
2026-05-05 19:33:52 -0500

    Added asteroid splitting and velocity change

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH as lw
+from constants import LINE_WIDTH as lw, ASTEROID_MIN_RADIUS as amr
+from logger import log_event
 
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        pygame.sprite.Sprite.kill(self)
+        if self.radius <= amr:
+            return
+        else:
+            log_event("asteroid_split")
+            new_angle = random.uniform(20, 50)
+            positive_vector = self.velocity.rotate(new_angle)
+            negative_vector = self.velocity.rotate(-new_angle)
+            new_radius = self.radius - amr
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity = positive_vector * 1.2
+            asteroid2.velocity = negative_vector * 1.2
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
                     pygame.sprite.Sprite.kill(shot)
-                    pygame.sprite.Sprite.kill(asteroid)
+                    asteroid.split()
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
